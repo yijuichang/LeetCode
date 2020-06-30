43d10f18fc14dd83009664887c34d558eb330378
yijuichang <michaelchang0928@gmail.com>
2020-06-30 10:49:55 -0700

    Update PIiaMA.cpp

--- PIiaMA.cpp
+++ PIiaMA.cpp
@@ -2,20 +2,20 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        int left=0;
-        int right=A.size()-1;
-        int mid=(right-left)/2;
-        while( left<right  ){
-            if(A[mid]<A[mid+1]){
-                left=mid+1;
-                mid=left+(right-left)/2;
+        int left = 0;
+        int right = A.size() - 1;
+        int mid= (right - left) / 2;
+        while (left < right){
+            if (A[mid] < A[mid+1]){
+                left = mid + 1;
+                mid = left + (right - left) / 2;
             }
-            else if(A[mid]<A[mid-1]){
-                right=mid-1;
-                mid=left+(right-left)/2;
+            else if (A[mid] < A[mid-1]){
+                right = mid - 1;
+                mid = left + (right - left) / 2;
             }
-            else{
-                left=right;
+            else {
+                left = right;
             }
         }
         return mid;
