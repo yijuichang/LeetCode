6fed82953f7eab961741a31c8995fc2e653c578d
yijuichang <michaelchang0928@gmail.com>
2020-07-11 00:10:34 -0700

    Create SameTree.cpp

---
+++ SameTree.cpp
@@ -0,0 +1,25 @@
+//100. Same Tree
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
+        if (p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Same Tree.
+Memory Usage: 9.9 MB, less than 58.84% of C++ online submissions for Same Tree.
+*/
